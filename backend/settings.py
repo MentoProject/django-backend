0bef9f795b5cd5ef9816cbdfd00943b9da893528
Mohammad Rehbar <md.rehbar@outlook.com>
2022-09-08 18:20:00 +0530

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -131,9 +131,9 @@ AUTH_USER_MODEL = 'auth_system.CustomUser'
 #Email settings
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "gitspacematrix@gmail.com"
+EMAIL_HOST_USER = 
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-EMAIL_HOST_PASSWORD = 'cldabqlxluainwxs'
+EMAIL_HOST_PASSWORD = 
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
